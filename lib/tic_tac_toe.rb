33bc856f45f0c160f4113d36806ec80be4a0f0a6
Natalia Castro <nat.alvz@gmail.com>
2018-05-08 03:07:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,14 @@ end
  end
  
  def turn 
-   
+     puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    return move(board, index, current_player(board))
+    display_board(board)
+  else turn(board)
+  end
  def turn_count
  
  end
