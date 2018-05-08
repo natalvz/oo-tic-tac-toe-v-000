e63cb7c84d0570427412dce35504946ed5655b85
Natalia Castro <nat.alvz@gmail.com>
2018-05-08 03:45:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,7 +75,7 @@ end
  
  def won?
    WIN_COMBINATIONS.detect do |win_combination|
-    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(index)(win_combination[0])
+    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(index, win_combination[0])
   end
 end
 
